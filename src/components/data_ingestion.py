4093597314a77bdf327b984a5a84ffc574ce532e
Mohit Rajput <Mohit.rajput010105@gmail.com>
2025-10-21 21:44:56 +0530

    Pipeline Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,15 +6,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformation       #These are the next pipeline stages, so the ingestion step can pass its
+                                                                        #output (train/test data) forward for transformation and model training
 from src.components.data_transformation import DataTransformationConfig
-
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    train_data_path: str = os.path.join('artifacts', 'train.csv')      
     test_data_path: str = os.path.join('artifacts', 'test.csv')
     raw_data_path: str = os.path.join('artifacts', 'data.csv')
 
